14e57a319c8d6e3d59d88c1d32751b8393ff7312
shrimpsooup <andrehamghalam@icloud.com>
2025-10-14 20:51:28 -0500

    Update comics.js

--- comics.js
+++ comics.js
@@ -1,5 +1,7 @@
 const COMICS_LIST = [
     {name: "pop", number: 1},
     {name: "ex_browser", number: 2},
-    {name: "abs_therapy", number: 3}
+    {name: "abs_therapy", number: 3},
+    {name: "osha_compliant", number: 4},
+    {name: "ai_takeover_2027", number: 5}
 ];
