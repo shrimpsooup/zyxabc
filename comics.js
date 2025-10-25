9540562c7f4f190c3ed4cea64036af633ae8dd83
shrimpsooup <andrehamghalam@icloud.com>
2025-10-24 20:57:50 -0500

    Update comics.js

--- comics.js
+++ comics.js
@@ -5,5 +5,6 @@ const COMICS_LIST = [
     {name: "osha_compliant", number: 4},
     {name: "ai_takeover", number: 5},
     {name: "bug_venn_diagram", number: 6},
-    {name: "wasted_plastic", number: 7}
+    {name: "wasted_plastic", number: 7},
+    {name: "the_hell_is_this_One_Cubic_Meter_Tungsten_Cube_doing_in_my_living_room", number: 8}
 ];
