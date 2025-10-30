af08fd66d9b69cb5e50470913ddd0b6d206e7fd7
shrimpsooup <andrehamghalam@icloud.com>
2025-10-29 20:19:45 -0500

    Update comics.js

--- comics.js
+++ comics.js
@@ -1,10 +1,10 @@
 const COMICS_LIST = [
-    {name: "pop", number: 1},
-    {name: "ex_browser", number: 2},
-    {name: "abs_therapy", number: 3},
-    {name: "osha_compliant", number: 4},
+    {name: "pop", number: 1, alt: "this one's pretty obscure to people not involved in physics. the joke is... pretty unclear in retrospect."},
+    {name: "ex_browser", number: 2, alt: "contrary to the last one, this is an experience most of us have had."},
+    {name: "abs_therapy", number: 3, alt: "math joke"},
+    {name: "osha_compliant", number: 4, alt: "the Grand Canyon, for all the people who can't understand pictures."},
     {name: "ai_takeover", number: 5},
-    {name: "bug_venn_diagram", number: 6},
-    {name: "wasted_plastic", number: 7},
-    {name: "the_hell_is_this_One_Cubic_Meter_Tungsten_Cube_doing_in_my_living_room", number: 8}
+    {name: "bug_venn_diagram", number: 6, alt: "some bugs are nice"},
+    {name: "wasted_plastic", number: 7, alt: "the lorax would not approve."},
+    {name: "the_hell_is_this_One_Cubic_Meter_Tungsten_Cube_doing_in_my_living_room", number: 8, alt: "wow, what a steal!"}
 ];
